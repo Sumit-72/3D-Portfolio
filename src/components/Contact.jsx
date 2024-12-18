c2aa3a3ee4cf245d5bf9756c09975b564e0a0496
Sumit Shekhar <shekharsumit65@gmail.com>
2024-12-18 23:43:08 +0530

    Cotact section fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,18 +5,18 @@ const Contact = () => {
   return (
     <div className="m-3">
         <WobbleCard
-        containerClassName="col-span-1 lg:col-span-2 h-full bg-pink-800 min-h-[400px] lg:min-h-[200px]"
+        containerClassName="col-span-1 lg:col-span-2 h-full bg-pink-800 min-h-[400px] lg:min-h-[200px] p-2"
         className=""
       >
       <div className=" w-auto h-auto mx-auto my-4 p-6 border bg-gray-200 dark:bg-black border-gray-300 rounded-lg shadow-xl transition-transform duration-300 ease-in-out transform hover:scale-105 hover:translate-x-2 hover:translate-y-2">
-        <p className="mb-2">
+        <p className="mb-2 p-1">
           <strong>Email:</strong>{" "}
           <a href="mailto:shekharsumit65@gmail.com">shekharsumit65@gmail.com</a>
         </p>
-        <p className="mb-2">
+        <p className="mb-2 p-1">
           <strong>Phone:</strong> <a href="tel:+919142845094">+91 9142845094</a>
         </p>
-        <p className="mb-2">
+        <p className="mb-2 p-1">
           <strong>LinkedIn:</strong>{" "}
           <a
             href="https://www.linkedin.com/in/sumit-shekhar72/"
@@ -27,7 +27,7 @@ const Contact = () => {
             https://www.linkedin.com/in/sumit-shekhar72/
           </a>
         </p>
-        <p className="mb-2">
+        <p className="mb-2 p-1">
           <strong>GitHub:</strong>{" "}
           <a
             href="https://github.com/Sumit-72"
