4eb7ca8ea2a9a0b13dca538262a990b64c3703a1
Sumit Shekhar <shekharsumit65@gmail.com>
2025-03-10 00:23:31 +0530

    Fix speed insight error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {Route,BrowserRouter as Router, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import {Home, About , Projects } from './pages'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const App = () => {
   return (
