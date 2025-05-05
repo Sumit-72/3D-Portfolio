13608e281ba98f173cec034fef83e70a811c5fb3
Sumit Shekhar <shekharsumit65@gmail.com>
2025-05-05 13:25:56 +0530

    Fix router error

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ const App = () => {
                 <Route path='/' element={<Home />}/>
                 <Route path='/about' element={<About />}/>
                 <Route path='/projects' element={<Projects />}/>
-
             </Routes>
             <Analytics/>\
             <SpeedInsights/>
